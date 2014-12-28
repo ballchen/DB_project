68b294b0311364fa55c131eff066d77541248fd0
Howard <how2945ard@gmail.com>
2014-12-28 17:12:31 +0800

    anti fake

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def current_user
-    @current_user ||= 1 && User.find_by_id(session[:current_user_id]) # Use find_by_id to get nil instead of an error if user doesn't exist
+    @current_user ||= session[:current_user_id] && User.find_by_id(session[:current_user_id]) # Use find_by_id to get nil instead of an error if user doesn't exist
   end
   helper_method :current_user #make this method available in views
 end
